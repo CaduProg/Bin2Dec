d319849272ce810957812b5945527e35d273bb58
CaduProg <carloseduardogcorrea11@gmail.com>
2023-10-17 15:55:49 -0300

    he

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Teste");
+        System.out.println("HAHAHHA");
+        System.out.println("HAHAHHE");
+        System.out.println("he");
+    }
+}
