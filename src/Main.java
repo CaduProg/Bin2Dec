6bcbc6d425bbc7e4ca753198b27825b04fc597ad
CaduProg <carloseduardogcorrea11@gmail.com>
2023-10-17 15:58:41 -0300

    teste da branch ha

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         System.out.println("Teste");
         System.out.println("HAHAHHA");
         System.out.println("HAHAHHE");
-        System.out.println("he");
+        System.out.println("ha");
     }
 }
