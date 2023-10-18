86ab7613dadf01b8013ec9d647ccb6c2e214c260
CaduProg <carloseduardogcorrea11@gmail.com>
2023-10-18 15:53:22 -0300

    Desenvolvido programa para transformar Binário a Decimal com o método parseInt

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Teste");
-        System.out.println("HAHAHHA");
-        System.out.println("HAHAHHE");
-        System.out.println("ha");
+    public static void main(String[] args) throws Exception {
+        Scanner leitura = new Scanner(System.in);
+            int i = 0;
+            while (i == 0) {
+                System.out.println("Digite até 8 números de 0 a 1, que podem ser intercalados ou não");
+                String numeros = leitura.next();
+                    if (numeros.length() <= 8) {
+
+                            try {
+                                int decimal = Integer.parseInt(numeros, 2);
+                                System.out.println(decimal);
+                            } catch (Exception e) {
+                                System.out.println("Caractere inválido");
+                            }
+                        }else{
+                        System.out.println("Ultrapassou a quantidade de caracteres!");
+                    }
+
+                System.out.println("Gostaria de parar o programa? 2 para sim e 3 para não");
+                Integer decisao = leitura.nextInt();
+                if (decisao == 2){
+                    break;
+                }
+
+            }
+        System.out.println("Programa finalizado com sucesso!");
+
+        }
+
     }
-}
